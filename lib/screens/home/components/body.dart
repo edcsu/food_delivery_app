e3bfa14505f34fec81ff8de5780ff9d367c5c329
Keith Ssewanonda <keith@laboremus.no>
2020-07-12 21:14:54 +0300

    feat: add category list

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,16 +1,18 @@
-
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/search_box.dart';
-import 'package:food_delivery_app/constants.dart';
+import 'package:food_delivery_app/screens/home/components/category_List.dart';
 
 class Body extends StatelessWidget {
-  
-    @override
-    Widget build(BuildContext context) {
-      return Column(
-        children: <Widget>[
-          SearchBox(onChanged: (value) => {},)
-        ],
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SearchBox(
+          onChanged: (value) => {},
+        ),
+        CategoryList()
+      ],
+    );
   }
+}
+
