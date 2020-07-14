947db249390802be28ba0e76bad20cbe41764856
Keith Ssewanonda <keith@laboremus.no>
2020-07-15 00:35:45 +0300

    style: add ratings on details page

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/constants.dart';
 import 'package:food_delivery_app/screens/details/components/details_appbar.dart';
 import 'package:food_delivery_app/screens/details/components/item_image.dart';
+import 'package:food_delivery_app/screens/details/components/title_price_rating.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key key}) : super(key: key);
@@ -40,34 +41,37 @@ class ItemInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(20),
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            ),
-            child: Column(
-              children: <Widget>[
-                shopName(name: "MacDonalds")
-              ],
-            ),
-            );
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          shopName(name: "MacDonalds"),
+          TitlePriceRating(
+            name: "Cheese Burger",
+            numOfReviews: 24,
+            price: 15,
+            rating: 4,
+            onRatingChanged: (rating) => {},
+          )
+        ],
+      ),
+    );
   }
 
   Row shopName({String name}) {
-    return Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: kSecondaryColor
-                  ),
-                  SizedBox(width: 10),
-                  Text(name),
-                ]
-              );
+    return Row(children: <Widget>[
+      Icon(Icons.location_on, color: kSecondaryColor),
+      SizedBox(width: 10),
+      Text(name),
+    ]);
   }
 }
+
 
